ec249635219812e7ecd258fbca9986e0bce02424
Elia Cunial <elia.cunial@gmail.com>
2024-07-14 19:30:15 +0200

    WIP

--- src/tree/scores.rs
+++ src/tree/scores.rs
@@ -5,9 +5,11 @@ use std::collections::HashMap;
 #[derive(Debug)]
 pub enum ScoreFunction {
     Gini,
+    Logit,
 }
 pub trait Score {
     fn gini(&self) -> f64;
+    fn logit(&self, prediction: f64) -> f64;
     fn score(&self, score_function: &ScoreFunction) -> f64;
     fn split_score(&self, filter_mask: &BooleanArray, score_function: &ScoreFunction) -> f64;
 }
@@ -31,9 +33,21 @@ impl Score for ArrayRef {
 
         sum_of_squares - 1.
     }
+    fn logit(&self, prediction: f64) -> f64 {
+        let boolean_array = self
+            .as_any()
+            .downcast_ref::<BooleanArray>()
+            .expect("Logit loss is valid only for boolean array");
+        boolean_array.iter().map(|target| match target {
+            Some(true) => 1.0,
+            Some(false) => 0.0,
+            _ => panic!("No idea how i can get here"),
+        }).map(|v| v * prediction.ln() + (1.-v) * (1. - prediction).ln()).sum()
+    }
     fn score(&self, score_function: &ScoreFunction) -> f64 {
         match score_function {
             ScoreFunction::Gini => self.gini(),
+            ScoreFunction::Logit => self.logit(0.5),
         }
     }
     fn split_score(&self, filter_mask: &BooleanArray, score_function: &ScoreFunction) -> f64 {
