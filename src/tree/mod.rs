e790e72d741dc368e40e69a3f7b7946aa77e671d
Elia Cunial <elia.cunial@gmail.com>
2024-09-08 16:37:16 +0200

    Refactor Split

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,3 +1,4 @@
 pub mod tree;
 mod scores;
 mod predictions;
+mod split;
--- src/tree/tree.rs
+++ src/tree/tree.rs
@@ -1,90 +1,11 @@
 use super::predictions::Prediction;
-use super::scores::{Score, ScoreFunction};
-use arrow::array::{Array, ArrayRef, BooleanArray, PrimitiveArray};
+use super::scores::ScoreFunction;
+use super::split::{best_split, SplitValue};
+use arrow::array::ArrayRef;
 use arrow::compute::{filter, filter_record_batch, not};
-use arrow::datatypes::{ArrowPrimitiveType, DataType, Float32Type, Float64Type, Int32Type};
 use arrow::record_batch::RecordBatch;
 use std::usize;
 
-#[derive(Debug, PartialEq)]
-pub enum SplitValue {
-    Numeric(f64),
-    String(String),
-}
-
-trait Split {
-    fn filter_mask(&self, split_point: &SplitValue) -> BooleanArray;
-    fn possible_splits_iter(&self) -> impl Iterator<Item = (SplitValue, BooleanArray)>;
-}
-
-impl<D> Split for PrimitiveArray<D>
-where
-    D: ArrowPrimitiveType,
-    D::Native: Into<f64>,
-{
-    fn filter_mask(&self, split_point: &SplitValue) -> BooleanArray {
-        if let SplitValue::Numeric(numeric_point) = split_point {
-            let split_point = *numeric_point;
-            BooleanArray::from(
-                self.values()
-                    .iter()
-                    .map(|&value| value.into() < split_point)
-                    .collect::<Vec<bool>>(),
-            )
-        } else {
-            panic!("Mismatched split point type for numeric array")
-        }
-    }
-    fn possible_splits_iter(&self) -> impl Iterator<Item = (SplitValue, BooleanArray)> {
-        self.values().iter().map(move |&split_point| {
-            (
-                SplitValue::Numeric(split_point.into()),
-                self.filter_mask(&SplitValue::Numeric(split_point.into())),
-            )
-        })
-    }
-}
-fn downcast_and_possible_splits<T>(
-    data_ref: &ArrayRef,
-) -> Box<dyn Iterator<Item = (SplitValue, BooleanArray)> + '_>
-where
-    T: ArrowPrimitiveType,
-    T::Native: Into<f64>,
-{
-    let array = data_ref
-        .as_any()
-        .downcast_ref::<PrimitiveArray<T>>()
-        .unwrap();
-    Box::new(array.possible_splits_iter())
-}
-
-fn best_split(
-    data: &RecordBatch,
-    target: ArrayRef,
-    split_function: &ScoreFunction,
-    prediction: Option<f64>,
-) -> Option<(f64, usize, BooleanArray, SplitValue)> {
-    let iters = (0..data.num_columns()).flat_map(|column_index| {
-        let col = data.column(column_index);
-        let target = target.clone(); // Clone the Arc, not the underlying data
-        match col.data_type() {
-            DataType::Float32 => downcast_and_possible_splits::<Float32Type>(col),
-            DataType::Float64 => downcast_and_possible_splits::<Float64Type>(col),
-            DataType::Int32 => downcast_and_possible_splits::<Int32Type>(col),
-            _ => panic!("Invalid data type"),
-        }
-        .filter_map(move |(split_value, filter_mask)| {
-            target
-                .split_score(&filter_mask, split_function, prediction)
-                .map(|split_score| (split_score, column_index, filter_mask, split_value))
-        })
-    });
-
-    iters.min_by(|a, b| {
-        a.0.partial_cmp(&b.0)
-            .expect(format!("Cannot compare {} with {}.", a.0, b.0).as_str())
-    })
-}
 
 #[derive(Debug, PartialEq)]
 pub struct Tree {
@@ -146,60 +67,11 @@ mod tests {
 
     use super::*;
     use arrow::{
-        array::{Float32Array, Int32Array},
+        array::{Float32Array, BooleanArray},
         datatypes::{DataType, Field, Schema},
     };
     use std::sync::Arc;
 
-    #[test]
-    fn test_best_split_ints() {
-        let my_schema: Arc<Schema> = Arc::new(Schema::new(vec![Field::new(
-            "sample",
-            DataType::Int32,
-            false,
-        )]));
-
-        let data: ArrayRef = Arc::new(Int32Array::from(vec![1, 2]));
-        let target: ArrayRef = Arc::new(BooleanArray::from(vec![true, false]));
-
-        let (_, row_index, filter_mask, threshold) = best_split(
-            &RecordBatch::try_new(my_schema, vec![data]).unwrap(),
-            target,
-            &ScoreFunction::Weighted(WeightedScoreFunction::Gini),
-            None,
-        )
-        .expect("No split found");
-
-        // Verify the splits' contents are as expected
-        assert_eq!(row_index, 0, "Wrong column index");
-        assert_eq!(filter_mask, BooleanArray::from(vec![true, false]));
-        assert_eq!(threshold, SplitValue::Numeric(2.), "Wrong threshold");
-    }
-    #[test]
-    fn test_best_split() {
-        let my_schema: Arc<Schema> = Arc::new(Schema::new(vec![Field::new(
-            "sample",
-            DataType::Float32,
-            false,
-        )]));
-
-        let data: ArrayRef = Arc::new(Float32Array::from(vec![1.0, 2.0]));
-        let target: ArrayRef = Arc::new(BooleanArray::from(vec![true, false]));
-
-        // Since best_split may return None, handle it properly
-        let (_, row_index, filter_mask, threshold) = best_split(
-            &RecordBatch::try_new(my_schema, vec![data]).unwrap(),
-            target,
-            &ScoreFunction::Weighted(WeightedScoreFunction::Gini),
-            None,
-        )
-        .expect("No split found");
-
-        // Verify the splits' contents are as expected
-        assert_eq!(row_index, 0, "Wrong column index");
-        assert_eq!(filter_mask, BooleanArray::from(vec![true, false]));
-        assert_eq!(threshold, SplitValue::Numeric(2.), "Wrong threshold");
-    }
 
     #[test]
     fn test_tree() {
